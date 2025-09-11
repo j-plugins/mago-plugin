b7a1a915d21c6ed0daad38dcf7008d720fdee6bd
Dmitriy Derepko <xepozz@list.ru>
2025-09-11 15:38:59 +0400

    refactor: temporary disable configurable

--- src/main/kotlin/com/github/xepozz/mago/MagoConfigurable.kt
+++ src/main/kotlin/com/github/xepozz/mago/MagoConfigurable.kt
@@ -2,12 +2,23 @@ package com.github.xepozz.mago
 
 import com.intellij.openapi.options.Configurable
 import com.intellij.openapi.project.Project
-import com.jetbrains.php.tools.quality.QualityToolProjectConfigurableForm
+import com.intellij.ui.dsl.builder.panel
 
-class MagoConfigurable(project: Project) : QualityToolProjectConfigurableForm(project), Configurable.NoScroll {
+//import com.jetbrains.php.tools.quality.QualityToolProjectConfigurableForm
+class MagoConfigurable(project: Project) : Configurable, Configurable.NoScroll {
+    override fun getDisplayName() = "Display Name"
+
     override fun getHelpTopic() = "reference.settings.php.mago"
 
-    override fun getId() = "settings.php.quality.tools.php.mago"
+//    override fun getId() = "settings.php.quality.tools.php.mago"
 
-    override fun getQualityToolType() = MagoQualityToolType.INSTANCE
+    //    override fun getQualityToolType() = MagoQualityToolType.INSTANCE
+    override fun createComponent() = panel {
+        row { label("Mago") }
+    }
+
+    override fun isModified() = false
+
+    override fun apply() {
+    }
 }
