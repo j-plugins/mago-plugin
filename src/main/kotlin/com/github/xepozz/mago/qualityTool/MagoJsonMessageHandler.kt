4fe8dab36b0e2e8e9daf38786fde4093c7af3a1c
Valentin Wotschel <valentin.wotschel@b-ite.de>
2026-02-09 13:23:18 +0100

    fix: improve detection of type-inspection annotations

--- src/main/kotlin/com/github/xepozz/mago/qualityTool/MagoJsonMessageHandler.kt
+++ src/main/kotlin/com/github/xepozz/mago/qualityTool/MagoJsonMessageHandler.kt
@@ -13,9 +13,10 @@ class MagoJsonMessageHandler {
             .getAsJsonArray("issues")
             ?.map { it.asJsonObject }
             ?.flatMap { issue ->
+                val code = issue.get("code")?.asString ?: ""
                 issue.getAsJsonArray("annotations")
                     ?.map { it.asJsonObject }
-                    ?.filter { it.get("kind").asString == "Primary" }
+                    ?.filter { it.get("kind").asString == "Primary" || code == "type-inspection" }
                     ?.mapNotNull { annotation ->
                         val span = annotation.getAsJsonObject("span") ?: return@mapNotNull null
                         val filePath = (span.getAsJsonObject("file_id")
@@ -25,14 +26,20 @@ class MagoJsonMessageHandler {
                             ?.let { FileUtil.toSystemIndependentName(it) }
                             ?: return@mapNotNull null)
 
+                        val message = if (annotation.get("kind").asString == "Secondary") {
+                            annotation.get("message").asString
+                        } else {
+                            "Mago: ${issue.get("message").asString.trimEnd('.')} [$code]"
+                        }
+
                         MagoProblemDescription(
                             levelToSeverity(issue.get("level").asString),
                             span.getAsJsonObject("start")?.get("line")?.asInt ?: return@mapNotNull null,
                             span.getAsJsonObject("start")?.get("offset")?.asInt ?: return@mapNotNull null,
                             span.getAsJsonObject("end")?.get("offset")?.asInt ?: return@mapNotNull null,
-                            "Mago: ${issue.get("message").asString.trimEnd('.')} [${issue.get("code").asString}]",
+                            message,
                             filePath,
-                            issue.get("code")?.asString ?: "",
+                            code,
                             issue.get("help")?.asString ?: "",
                             issue.getAsJsonArray("notes")?.map { it.asString } ?: emptyList(),
                         )
